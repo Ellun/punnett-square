0f4b663f981ebaddffc85decf088575dfa6f1880
Elton Cheng <eltoncheng86@gmail.com>
2016-04-09 14:21:52 -0400

    added temp instructions

--- public/js/components/instructions.js
+++ public/js/components/instructions.js
@@ -10,7 +10,24 @@ const Instructions = React.createClass({
     return (
       <div id="instructions">
         <h1 className="header">Instructions</h1>
-        <p> Stop sucking at games </p>
+        <p> Golems are being bombarded with extreme weather and habitats.
+        Help these poor critters selectively breed to survive these harsh terrains!
+        </p>
+        <p>
+        Drag and drop the parents in the drop slots.
+        <img src="../../images/dropstonedude.png"/>
+        </p>
+        <p>
+        Use the punnett square to determine what your children will look like.
+        </p>
+        <p>
+        Golems come in Stone, Ice, or Lava form. What determines this is their genetic makeup.
+        Lava golems have a genetic makeup of (LL).
+        Ice golems have a genetic makeup of (II).
+        Stone golems is anything else!
+        </p>
+
+
         <Link id="scoresHome" to="/home">Menu</Link>
       </div>
     )
